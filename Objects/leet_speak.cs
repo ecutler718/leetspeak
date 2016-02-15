e8d582d242800c73e6748a167e969f3b2fd83665
michael smith <michaelanthonysmith00@gmail.com>
2016-02-15 13:37:05 -0800

    wrote first passing test, not sure why it's passing

--- Objects/leet_speak.cs
+++ Objects/leet_speak.cs
@@ -2,8 +2,28 @@ using System.Collections.Generic;
 
 namespace LeetSpeak
 {
-  public class Translator
+  public class Sentence
   {
     //code goes here
+    private string _userPhrase;
+
+    public Sentence(string userPhrase)
+    {
+      _userPhrase = userPhrase;
+    }
+
+    public string GetUserPhrase()
+    {
+      return _userPhrase;
+    }
+    public void SetUserPhrase(string userPhrase)
+    {
+      _userPhrase = userPhrase;
+    }
+
+    // public static Translate(Sentence userPhrase)
+    // {
+    //   return userPhrase;
+    // }
   }
 }
--- Tests/TranslateTest.cs
+++ Tests/TranslateTest.cs
@@ -7,6 +7,19 @@ namespace Translator
 {
   public class TranslateTest
   {
-  //First test
+    //First test: will take a phrase from the user that it will translate.
+    [Fact]
+    public void test1GetPhraseFromUser()
+    {
+      //Arrange
+      string phrase = "Walk the dog";
+      Sentence newSentence = new Sentence(phrase);
+
+      //Act
+      string result = "Walk the dog";
+
+      //Assert
+      Assert.Equal(phrase, result);
+    }
   }
 }
