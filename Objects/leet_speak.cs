5b1029143709d1c0d66edd955c714c2a270c74ee
michael smith <michaelanthonysmith00@gmail.com>
2016-02-15 13:16:48 -0800

    initial commit, setup project folders. Will start writing test now.

---
+++ Objects/leet_speak.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace LeetSpeak
+{
+  public class Translator
+  {
+    //code goes here
+  }
+}
---
+++ Tests/TranslateTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using LeetSpeak;
+using System;
+using System.Collections.Generic;
+
+namespace Translator
+{
+  public class TranslateTest
+  {
+  //First test
+  }
+}
